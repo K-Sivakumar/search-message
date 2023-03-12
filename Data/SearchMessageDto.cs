724f20b653342a770286d9b2164dc0c30c099bc8
Sivakumar <sivakumar29@gmail.com>
2023-03-12 20:04:23 +0530

    initial code base

---
+++ Data/SearchMessageDto.cs
@@ -0,0 +1,15 @@
+﻿namespace SearchMessage.Data
+{
+    public class SearchMessageDto
+    {
+        public DateTime ReceivedDate { get; set; }
+
+        public string? ReceivedFrom { get; set; }
+
+        public string Summary { get; set; }
+
+        public string? Subject { get; set; }
+
+        public int? Rank { get; set; }
+    }
+}
---
+++ Data/SearchMessageService.cs
@@ -0,0 +1,51 @@
+﻿namespace SearchMessage.Data
+{
+    using Microsoft.Graph;
+
+    public class SearchMessageService
+    {
+        private readonly GraphServiceClient graphClient;
+
+        public SearchMessageService(GraphServiceClient graphClient)
+        {
+            this.graphClient = graphClient;
+        }
+
+        public async Task<IEnumerable<SearchMessageDto>> SearchMessagesAsync(string value)
+        {
+            var requests = new List<Task<ISearchEntityQueryCollectionPage>>
+            {
+                CreateSearchQueryObject(EntityType.Event, value),
+                CreateSearchQueryObject(EntityType.Message, value),
+                CreateSearchQueryObject(EntityType.ChatMessage, value),
+            };
+            var results = await Task.WhenAll(requests);
+            var searchHits = results.SelectMany(res => res.Where(res => res.HitsContainers != null)
+                                        .SelectMany(res => res.HitsContainers.Where(res => res.Hits != null)
+                                            .SelectMany(res => res.Hits)));
+            return searchHits.Select(sr => new SearchMessageDto
+            {
+                Rank = sr.Rank,
+                Summary = sr.Summary
+            });
+
+            Task<ISearchEntityQueryCollectionPage> CreateSearchQueryObject(EntityType entityType, string value)
+            {
+                return graphClient.Search.Query(new List<SearchRequestObject>
+                {
+                    new SearchRequestObject
+                    {
+                        EntityTypes = new List<EntityType>
+                        {
+                            entityType
+                        },
+                        Query = new SearchQuery
+                        {
+                            QueryString = value
+                        }
+                    }
+                }).Request().PostAsync();
+            }
+        }
+    }
+}
